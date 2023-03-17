52a00543797e122047b4fd407239924bb9a2b4e4
Yunhee Kim <yunhee858@naver.com>
2023-03-17 17:46:40 +0900

    Python basic, module,graph

---
+++ 0317_python_class/greet.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# python module
+
+# In[1]:
+
+
+def hello (name):
+    print('{}님 안녕하세요'.format(name))
+def niceMeet(name):
+    print('{}님 만나서 반갑습니다'.format(name))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
